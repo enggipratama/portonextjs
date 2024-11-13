92d0bd50eeb5837a0273d65f183abc39a065fbb8
enggipratama <36240898+enggipratama@users.noreply.github.com>
2024-11-13 19:27:35 +0700

    Styling

--- src/app/components/login/formlogin.tsx
+++ src/app/components/login/formlogin.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { FiMail, FiLock } from "react-icons/fi";
 const Login = () => {
   return (
     <div className="hero min-h-screen flex items-center justify-center px-5">
@@ -15,16 +16,40 @@ const Login = () => {
           <h2 className="card-title mb-3 text-center lg:text-left">
             Login Dashboard
           </h2>
-          <label className="input input-bordered flex items-center gap-2 mb-3">
-            <span className="text-sm lg:text-base">Email</span>
-            <input type="text" className="grow p-2 rounded-md" />
-          </label>
-          <label className="input input-bordered flex items-center gap-2 mb-3">
-            <span className="text-sm lg:text-base">Password</span>
-            <input type="password" className="grow p-2 rounded-md" />
-          </label>
+          <div>
+            {/* Email Input */}
+            <label className="input input-bordered flex items-center gap-2 mb-3">
+              <FiMail className="text-xl" />
+              <input
+                placeholder="Email"
+                type="text"
+                className="grow p-2 rounded-md"
+              />
+            </label>
+
+            {/* Password Input */}
+            <label className="input input-bordered flex items-center gap-2 mb-3">
+              <FiLock className="text-xl" />
+              <input
+                placeholder="Password"
+                type="password"
+                className="grow p-2 rounded-md"
+              />
+            </label>
+          </div>
           <div className="card-actions justify-center lg:justify-end mt-3">
-            <button className="btn btn-primary w-full lg:w-auto">Login</button>
+            <a
+              href="/"
+              className="font-bold text-secondary w-full lg:w-auto px-4 py-2 border border-secondary rounded-md hover:bg-secondary hover:text-white transition-all"
+            >
+              Back
+            </a>
+            <a
+              href="URL_TUJUAN"
+              className="font-bold text-primary w-full lg:w-auto px-4 py-2 border border-primary rounded-md hover:bg-primary hover:text-white transition-all"
+            >
+              Login
+            </a>
           </div>
         </div>
       </div>
