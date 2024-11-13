2733ed8eb9de6e2f14896425e28a5cc5e863735c
enggipratama <36240898+enggipratama@users.noreply.github.com>
2024-11-13 19:33:50 +0700

    up

--- src/app/components/home/buttonviewnav.tsx
+++ src/app/components/home/buttonviewnav.tsx
@@ -8,9 +8,9 @@ const ViewButton = () => {
         <div className="text-center mt-5 w-full z-10">
         <div className="join">
             <div>
-                <Link href="/" className="btn join-item font-bold text-primary w-full lg:w-auto px-4 py-2 rounded-md hover:bg-primary hover:text-white text-white transition-all">Home</Link>
-                <Link href="/project" className="btn join-item font-bold text-primary w-full lg:w-auto px-4 py-2 rounded-md hover:bg-primary hover:text-white text-white transition-all">Project</Link>
-                <Link href="/login" className="btn join-item font-bold text-primary w-full lg:w-auto px-4 py-2 rounded-md hover:bg-primary hover:text-white text-white transition-all">Login</Link>
+                <Link href="/" className="btn join-item font-bold text-primary lg:w-auto px-4 py-2 rounded-md hover:bg-primary hover:text-white text-white transition-all">Home</Link>
+                <Link href="/project" className="btn join-item font-bold text-primary lg:w-auto px-4 py-2 rounded-md hover:bg-primary hover:text-white text-white transition-all">Project</Link>
+                <Link href="/login" className="btn join-item font-bold text-primary lg:w-auto px-4 py-2 rounded-md hover:bg-primary hover:text-white text-white transition-all">Login</Link>
             </div>
         </div>
     </div>
